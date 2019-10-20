5e86f92e2c58129031a4783a16e04352298f559c
Ketan D Khanolkar <khanolkarketan645@gmail.com>
2019-10-20 19:38:49 +0530

    hey I have commited all the changes

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\khano\desktop\codeforces\django_project\django-theatre-ticket-booking\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
